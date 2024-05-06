2689c9dc9552602c3de4438fad8d73ea488ece9d
Ryan Bai <ryanbai1412@gmail.com>
2024-05-06 01:30:22 -0700

    rename project to cmp

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use shuttle_persist::PersistInstance;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_persist::Persist] persist: PersistInstance) -> shuttle_axum::ShuttleAxum {
-    Ok(warren_cmp::app(persist).into())
+    Ok(cmp::app(persist).into())
 }
